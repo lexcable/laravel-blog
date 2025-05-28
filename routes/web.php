ab5fc28b18886667eb86d2bbf418b6f695cbcdbd
lexcable <lexc92603@gmail.com>
2025-05-28 10:56:59 -0600

    Update: latest changes and fixes

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,14 @@
 use App\Models\Post;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-
-Route::get('/posts', function () {
-    $posts = Post::all();
-    return view('posts.index', compact('posts'));
+Route::get('/', function () {
+    return view('layout');
 });
+
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
@@ -18,5 +18,10 @@ Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-;
 
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
+
+Route::fallback(function () {
+    return response()->view('errors.404', [], 404);
+});
